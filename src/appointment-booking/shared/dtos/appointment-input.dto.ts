465f4f406484b6201b618fa3562d36f03c452f7f
Karim Tayie <karimsherif2597@gmail.com>
2025-01-21 01:53:20 +0200

    feat: enhance UpdateAppointmentInput to include status enum and default value

--- src/appointment-booking/shared/dtos/appointment-input.dto.ts
+++ src/appointment-booking/shared/dtos/appointment-input.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
+import { AppointmentStatus } from '../../internal/infrastructure/db/appointment.entity';
 import { AppointmentStatusType } from '../interfaces/appointment-status.type';
 
 export class UpdateAppointmentInput {
-  @ApiProperty()
+  @ApiProperty({
+    enum: AppointmentStatus,
+    default: AppointmentStatus.COMPLETED,
+  })
   @IsNotEmpty()
   status: AppointmentStatusType;
 }
